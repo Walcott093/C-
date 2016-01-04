dae3de9b970a72df3c3dd8c91fd1434f47c71c83
C-V-K-D <dakhliacyrille@gmail.com>
2016-01-04 23:09:32 +0100

    Nettoyage repertoires

--- cvkd/architectureGenerique/BANK/test/A.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "A.hpp"
-using namespace std;
-
-A::A() {cout<<"A cree"<<endl;}
-A::~A() {cout<<"A detruit"<<endl;}
-
-string A::getA() {return a;}
--- cvkd/architectureGenerique/BANK/test/B.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "B.hpp"
-using namespace std;
-
-B::B(): A() {cout<<"B cree"<<endl;}
-B::~B() {cout<<"B detruit"<<endl;}
-
-string B::getB() {return b;}
